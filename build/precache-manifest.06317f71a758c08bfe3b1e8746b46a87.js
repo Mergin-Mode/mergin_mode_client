2e0be63fe7bd70a74e62d62fcd1835eac3e84c91
Dr <priestont@gmail.com>
2020-09-15 21:52:49 +0300

    initialize

---
+++ build/precache-manifest.06317f71a758c08bfe3b1e8746b46a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6896ca030bee845ed20cda99e799eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d118366277a68dadfdb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d118366277a68dadfdb",
+    "url": "/static/js/main.bbcbe1fd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <center>
+      <h4>Mergin Mode</h4>
+      <h6>Client</h6>
+      </center>
+    </div>
+  );
+}
+
+export default App;
