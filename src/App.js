cef8f14c79d674ac77ecb4d4c4c1eb4b78a5a263
Dr <priestont@gmail.com>
2020-09-19 15:12:25 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useRef,useState,useEffect } from 'react';
+import React, { useRef,useState,useEffect,PropTypes } from 'react';
 import './App.css';
 import Webcam from "react-webcam";
 import createWorld from "./helpers/createWorld.js";
+
 window.mergin_mode = {
   modelLayer: [],
   vectors:[],
@@ -17,33 +18,38 @@ function App() {
   const [elements, setElements] = useState({
     mixers:[], camera:{}, controls:{}, scene:{}, renderer:{},pointer:{}, partials:{},loaders:{},onWindowResize:()=>{}
   })
+  const [position,setPosition] = useState({});
+  const [rotation,setRotation] = useState({});
+
   useEffect(()=>{
-   
+    if (window.Accelerometer){
+      let acl = new window.Accelerometer({frequency: 60});
+
+      acl.addEventListener('reading', () => {
+        setPosition({x:acl.x.toFixed(2),y:acl.y.toFixed(2),z:acl.z.toFixed(2)})
+        console.log("Acceleration along the X-axis " + acl.x);
+        console.log("Acceleration along the Y-axis " + acl.y);
+        console.log("Acceleration along the Z-axis " + acl.z);
+      });
+
+      acl.start();
+    }
+    
+    window.addEventListener("deviceorientation", function handleOrientation(event) {
+      var absolute = event.absolute;
+      var alpha    = event.alpha.toFixed(2);
+      var beta     = event.beta.toFixed(2);
+      var gamma    = event.gamma.toFixed(2);
+      setRotation({x:beta,y:gamma,z:alpha})
+
+      // Do stuff with the new orientation data
+    }, true);
     navigator.mediaDevices.getUserMedia(constraints).then(response=>{
       const video = document.getElementById("video");
       video.srcObject = response;
       video.play();
       const settings = response.getTracks()[0].getSettings()
       setCameraSettings({...settings});
-      // window.addEventListener('resize',()=> {
-      //   const windowAspectRatio = window.innerWidth/window.innerHeight;
-      //   if( windowAspectRatio < settings.aspectRatio ){
-      //       setScale(window.innerWidth/settings.width)
-      //       // setCameraSettings({
-      //       //   ...cameraSettings,
-      //       //   width:window.innerWidth,
-      //       //   height:window.innerWidth/settings.width * settings.height
-      //       // })
-      //   }else {
-      //       setScale(window.innerHeight/settings.height)
-      //       // setCameraSettings({
-      //       //   ...cameraSettings,
-      //       //   height:window.innerHeight,
-      //       //   width:window.innerHeight/settings.height * settings.width
-      //       // })
-      //   }
-      // });
-      // window.dispatchEvent( new Event('resize') );
       let {mixers,camera,controls,scene,renderer,pointer,partials,loaders} = elements;
       const rendererContainer = document.getElementById("three-map");
       const newWorld = createWorld(camera,controls,scene,renderer,pointer,partials,loaders,rendererContainer,mixers);
@@ -67,8 +73,10 @@ function App() {
           position:"absolute",
           width:`100%`,
           height:`100%`,
-        }}></div>   
+        }}></div>
       </div>
+      <div style={{position:"absolute",top: 0,left: 0}}>{"Accelerometer " + JSON.stringify(position)}</div>
+      <div style={{position:"absolute",top: "20px",left: 0}}>{"Orientation " + JSON.stringify(rotation)}</div>
     </div>
   );
 }
