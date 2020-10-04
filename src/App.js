3c18aab5a4d3741894e34f63812801d51180f43e
Dr <priestont@gmail.com>
2020-10-04 21:25:00 +0300

    progress with accelarator

--- src/App.js
+++ src/App.js
@@ -10,52 +10,85 @@ window.mergin_mode = {
   sky:{},
   mixers:[]
 }
+
+function calcPosition(s=0,u=0){
+  if (u==0) return Number(s);
+  return Number(s)+Number(u)*1/60;
+}
+
+function calcVelocity(u=0,ang=0,a=0){
+  if(a==0) return 0;
+  return Number(u)+(Math.cos(Number(ang))*Number(a))*1/60;
+}
 function App() {
   const webcamRef = useRef(null);
   const [cameraSettings,setCameraSettings] = useState(null);
-  const [scale,setScale] = useState(1);
   const constraints = { video: { width: 1280, height: 720, facingMode: 'environment' } };
   const [elements, setElements] = useState({
     mixers:[], camera:{}, controls:{}, scene:{}, renderer:{},pointer:{}, partials:{},loaders:{},onWindowResize:()=>{}
   })
-  const [position,setPosition] = useState({});
-  const [rotation,setRotation] = useState({});
+  const [rotation,setRotation] = useState({x:0,y:0,z:0});
+  const [velocity,setVelocity] = useState({x:0,y:0,z:0});
+  const [position,setPosition] = useState({x:0,y:0,z:0});
+  const [acceleration,setACL] = useState({x:0,y:0,z:0});
+  let pos = useRef(position);
+  let vel = useRef(velocity);
+  let rot = useRef(rotation);
 
   useEffect(()=>{
-    if (window.LinearAccelerationSensor){
-      let acl = new window.LinearAccelerationSensor({frequency: 60});
 
-      acl.addEventListener('reading', () => {
-        setPosition({x:acl.x.toFixed(2),y:acl.y.toFixed(2),z:acl.z.toFixed(2)})
-        console.log("Acceleration along the X-axis " + acl.x);
-        console.log("Acceleration along the Y-axis " + acl.y);
-        console.log("Acceleration along the Z-axis " + acl.z);
+    // if (window.LinearAccelerationSensor){
+    //   let acl = new window.LinearAccelerationSensor({frequency: 60});
+    //   window.addEventListener("deviceorientation", function handleOrientation(event) {
+    //     var absolute = event.absolute;
+    //     var alpha    = Number(event.alpha || 0);
+    //     var beta     = Number(event.beta || 0);
+    //     var gamma    = Number(event.gamma|| 0);
+    //     rot.current = {x:beta,y:gamma,z:alpha};
+    //     setRotation({x:rot.current.x.toFixed(2),y:rot.current.y.toFixed(2),z:rot.current.z.toFixed(2)})
+
+    //     // Do stuff with the new orientation data
+    //   }, true);
+
+      navigator.mediaDevices.getUserMedia(constraints).then(response=>{
+        const video = document.getElementById("video");
+        video.srcObject = response;
+        video.play();
+        const settings = response.getTracks()[0].getSettings()
+        setCameraSettings({...settings});
+        let {mixers,camera,controls,scene,renderer,pointer,partials,loaders} = elements;
+        const rendererContainer = document.getElementById("three-map");
+        const newWorld = createWorld(camera,controls,scene,renderer,pointer,partials,loaders,rendererContainer,mixers);
+
+        navigator.geolocation.getCurrentPosition(function success(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setPosition({lat:latitude,long:longitude})
+  }, ()=>{});
       });
 
-      acl.start();
-    }
+    //   acl.addEventListener('reading', () => {
+    //     vel.current = {
+    //       x:calcVelocity(vel.current.x,rot.current.x,acl.x),
+    //       y:calcVelocity(vel.current.y,rot.current.y,acl.y),
+    //       z:calcVelocity(vel.current.z,rot.current.z,acl.z)
+    //     }
+    //     setVelocity({x:vel.current.x.toFixed(2),y:vel.current.y.toFixed(2),z:vel.current.z.toFixed(2)})
+    //     pos.current = {
+    //       x:calcPosition(pos.current.x,vel.current.x),
+    //       y:calcPosition(pos.current.y,vel.current.y),
+    //       z:calcPosition(pos.current.z,vel.current.z),
+    //     }
+    //     setPosition({x:pos.current.x.toFixed(2),y:pos.current.y.toFixed(2),z:pos.current.z.toFixed(2)})
+    //     setACL({x:acl.x,y:acl.y,z:acl.z})
+    //   });
+
+    //   acl.start();
+    // }
     
-    window.addEventListener("deviceorientation", function handleOrientation(event) {
-      var absolute = event.absolute;
-      var alpha    = event.alpha.toFixed(2);
-      var beta     = event.beta.toFixed(2);
-      var gamma    = event.gamma.toFixed(2);
-      setRotation({x:beta,y:gamma,z:alpha})
+    
 
-      // Do stuff with the new orientation data
-    }, true);
-    navigator.mediaDevices.getUserMedia(constraints).then(response=>{
-      const video = document.getElementById("video");
-      video.srcObject = response;
-      video.play();
-      const settings = response.getTracks()[0].getSettings()
-      setCameraSettings({...settings});
-      let {mixers,camera,controls,scene,renderer,pointer,partials,loaders} = elements;
-      const rendererContainer = document.getElementById("three-map");
-      const newWorld = createWorld(camera,controls,scene,renderer,pointer,partials,loaders,rendererContainer,mixers);
-    });
-
-  },[])
+  },[constraints, elements])
 
 
 
@@ -75,8 +108,10 @@ function App() {
           height:`100%`,
         }}></div>
       </div>
-      <div style={{position:"absolute",top: 0,left: 0}}>{"Accelerometer " + JSON.stringify(position)}</div>
-      <div style={{position:"absolute",top: "20px",left: 0}}>{"Orientation " + JSON.stringify(rotation)}</div>
+      {/*<div style={{position:"absolute",top: 0,left: 0,background:"white"}}>{"Rotation " + JSON.stringify(rotation)}</div>
+      <div style={{position:"absolute",top: "20px",left: 0,background:"white"}}>{"Velocity " + JSON.stringify(velocity)}</div>
+      <div style={{position:"absolute",top: "60px",left: 0,background:"white"}}>{"ACL " + JSON.stringify(acceleration)}</div>*/}
+      <div style={{position:"absolute",top: "40px",left: 0,background:"white"}}>{"Position " + JSON.stringify(position)}</div>
     </div>
   );
 }
